0e6e8208ed5441345e13f6f74b50d3322d2f6d6d
benedikt kneussl <benediktkneussl@gmail.com>
2019-10-07 19:09:11 +0800

    added stripe customer token to users table

--- db/migrate/20191007110609_add_stripe_customer_token_to_users.rb
+++ db/migrate/20191007110609_add_stripe_customer_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddStripeCustomerTokenToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :stripe_customer_token, :strings
+    add_column :users, :stripe_customer_token, :string
   end
 end
